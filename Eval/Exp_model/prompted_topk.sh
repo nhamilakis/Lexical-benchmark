1874136434ad1a975bf40833a2ee6c4c9c69ca31
Jing Liu <84009338+Jing-L97@users.noreply.github.com>
2023-12-09 16:20:28 +0100

    Delete Eval/Exp_model/prompted_topk.sh

--- Eval/Exp_model/prompted_topk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-# Add more python commands for additional files if needed
-#python random_generation_new.py --month 1
-python prompted_generation_new.py --month 3 
-python prompted_generation_new.py --month 12 
