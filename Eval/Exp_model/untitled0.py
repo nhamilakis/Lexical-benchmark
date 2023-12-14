8bd0ffa0eea5683af69696b8f4ec42753d77ef41
Jing Liu <84009338+Jing-L97@users.noreply.github.com>
2023-12-14 20:19:17 +0100

    Add files via upload

--- Eval/Exp_model/untitled0.py
+++ Eval/Exp_model/untitled0.py
@@ -4,17 +4,25 @@
 sample the similar amount of generations as the CHILDES distribution
 
 @author: jliu
+
+
+TO DO: 
+    1. match lemmas: stict match
+
+    2. get the ranges using the artificial baby
 """
 
 import os
 import pandas as pd
-from plot_entropy_util import plot_single_para, plot_distance, match_seq,lemmatize,filter_words,get_score
 import collections
 import argparse
-import sys
+from accum_util import *
 import seaborn as sns
 import matplotlib.pyplot as plt
+import enchant
+from plot_entropy_util import lemmatize,get_score
 
+d = enchant.Dict("en_US")
 
 def parseArgs(argv):
     # Run parameters
@@ -47,11 +55,16 @@ def parseArgs(argv):
     return parser.parse_args(argv)
 
 
-
+word_path = 'target_words'
 root_path = 'generation'
+prompt_type = 'prompted'
+strategy = 'sample_topp'
+word_per_hour = 10000
+hour = 1
 
-
-def load_data(root_path, prompt_type,strategy):
+def load_data(root_path, prompt_type,strategy,word_per_hour,hour):
+    
+    sns.set_style('whitegrid')
     
     '''
     count all the words in the generated tokens 
@@ -65,14 +78,14 @@ def load_data(root_path, prompt_type,strategy):
     month_dict = {'50h':[1],'100h':[1],'200h':[2,3],'400h':[4,8],'800h':[9,18],'1600h':[19,28],'3200h':[29,36]}
     
     generation_all = pd.DataFrame()
+    seq_all = []
     # go over the generated files recursively
     for month in os.listdir(root_path): 
             
         for chunk in os.listdir(root_path + '/' + month): 
             
-            for file in os.listdir(root_path + '/' + month + '/' +  chunk+ '/' + prompt_type + '/' + strategy):
-                        
-                try:
+            try:
+                for file in os.listdir(root_path + '/' + month + '/' +  chunk+ '/' + prompt_type + '/' + strategy):
                         
                     # load decoding strategy information       
                     data = pd.read_csv(root_path + '/' + month + '/' +  chunk+ '/' + prompt_type + '/' + strategy + '/' + file)
@@ -80,134 +93,229 @@ def load_data(root_path, prompt_type,strategy):
                     # sample data of the corresponding month
                     result = data.loc[(data['month'] >= month_dict[month][0]) & (data['month'] <= month_dict[month][-1])]
                     generation_all = pd.concat([generation_all,result])   
+                    
+                    n = 0
+                    while n < result.shape[0]:
+                        generated = result['LSTM_segmented'].tolist()[n].split(' ')
+                        seq_all.extend(generated)
+                        n += 1
+                        
                     print('SUCCESS: ' + month+ '/' + prompt_type + '/' + strategy + '/' + file)    
                                     
-                except:
-                    print('FAILURE: ' + month+ '/' + prompt_type + '/' + strategy + '/' + file)
-                                
-     # get normalized frequency by each month
-     for month in generation_all['month'].tolist():
-         selected_frame = generation_all[generation_all['month']==month]
-         # normalize word count in the given month
-         
-         seq = []
-         n = 0
-         while n < selected_frame.shape[0]:
-             generated = selected_frame['LSTM_segmented'].tolist()[n].split(' ')
-             seq.extend(generated)
-             n += 1
-                
-         # get freq lists
-         frequencyDict = collections.Counter(seq)  
-         freq_lst = list(frequencyDict.values())
-         word_lst = list(frequencyDict.keys())
-         fre_table = pd.DataFrame([word_lst,freq_lst]).T           
-         col_Names=["Word", "Freq"]
-         fre_table.columns = col_Names
-     
-         # get the frequency that corresponds to each month
-         
-         seq_all.extend(seq)
-     
-        # get word count and lemma count frames
-        seq_lst = list(set(seq_all))
+            except:
+                    print('FAILURE: ' + month+ '/' + prompt_type + '/' + strategy)
+    
+    # get the list of all the words
+    seq_lst_temp = list(set(seq_all))
+    seq_lst = [element for element in seq_lst_temp if element.strip() != '']   
+    cleaned_frame = pd.DataFrame(seq_lst)
+    
+    for i in list(set(generation_all['month'].tolist())):
+        cleaned_frame[i] = [0] * len(seq_lst)
+    
+    cleaned_frame.set_index(0, inplace=True)
+    
+    # Fill the DataFrame with zeros for the specified number of rows
+    for month in list(set(generation_all['month'].tolist())):
+        selected_frame = generation_all[generation_all['month']==month]
+        seq = []
+        n = 0
+        while n < selected_frame.shape[0]:
+            generated = selected_frame['LSTM_segmented'].tolist()[n].split(' ')
+            seq.extend(generated)
+            n += 1
+        # get the frequency that corresponds to each month
+        filtered_seq = [element for element in seq if element.strip() != '']       
+        # get freq lists
+        frequencyDict = collections.Counter(filtered_seq)  
         
-        seq_frame = match_seq(seq_lst,frame_all)
+        for word, freq in frequencyDict.items():
+                # append the list to the frame 
+                cleaned_frame.loc[word,month] = freq/len(filtered_seq)* 30 * word_per_hour * hour
+                n += 1
     
-    word_lst, lemma_dict = lemmatize(seq_lst)
+    # get cumulative frequency
+    seq_frame = cleaned_frame.cumsum(axis=1)
     
-    word_lst.extend(['MONTH','CHUNK','PROMPT','BEAM','TOPK','TEMP','TOPP','RANDOM','DECODING'])
-    word_frame = seq_frame[word_lst]
+    # get the lemam frame and then match the results
+    lemma_dict = lemmatize(seq_lst)
     
-    # reshape the lemma frame based onthe word_frame: basic info, lemma, total counts
-    lemma_frame = seq_frame[['MONTH','CHUNK','PROMPT','BEAM','TOPK','TEMP','TOPP','RANDOM','DECODING']]
+    lemma_frame = pd.DataFrame()
     for lemma, words in lemma_dict.items():
         # Merge columns in the list by adding their values
-        lemma_frame[lemma] = word_frame[words].sum(axis=1)
-        
-        norm_count = fre_table[fre_table['Word']==word]['Norm_freq'].item() * 30 * word_per_hour * hour/
-    return seq_frame, word_frame, lemma_frame, word_freq_frame
-   
-
-
-
-
-
-
-# sample data based on months
-
-
-# check the overlap between wordbank and those for receptive vocab
-vocab_filtered = pd.read_csv(root_path + '/vocab_filtered.csv')
-vocab_unfiltered = pd.read_csv(root_path + '/vocab_unfiltered.csv')
-AE_CDI = pd.read_csv(root_path + '/AE_content.csv')
-BE_CDI = pd.read_csv(root_path + '/BE_content.csv')
-
-
-
-
-# plot the vocab size curve based on word counts
-target_frame = vocab_filtered[['word','group_3']]
-
-# rename the column header
-target_frame = target_frame.rename(columns={target_frame.columns[1]: 'freq'})
-
-
-# plot the vocab size curve based on word counts
-target_frame = vocab_unfiltered[['word','group']]
-
-# rename the column header
-target_frame = target_frame.rename(columns={target_frame.columns[1]: 'freq'})
-
-
-def plot_curve(threshold,input_frame,target_frame,by_freq,info_lst,prompt,decoding,seq_type):
+        # Sum all rows to get one row with summed values
+        df = seq_frame.loc[words]
+        summed_row = df.sum(axis=0)
+        # Convert the resulting Series back to a DataFrame with one row
+        summed_df = pd.DataFrame([summed_row], columns=summed_row.index)
+        lemma_frame = pd.concat([lemma_frame,summed_df])
     
-    '''
-    input: the frame with word vocab size score
+    lemma_frame.index = list(lemma_dict.keys())
 
-    Returns
-    the learning curve of the figure
+    return seq_frame, lemma_frame
+   
+seq_frame_prompted,lemma_frame_prompted = load_data(root_path, 'prompted',strategy,word_per_hour,hour)
+seq_frame_unprompted,lemma_frame_unprompted = load_data(root_path, 'unprompted',strategy,word_per_hour,hour)
 
-    '''
+
+vocab_filtered = pd.read_csv(word_path + '/vocab_filtered.csv')
+vocab_unfiltered = pd.read_csv(word_path + '/vocab_unfiltered.csv')
+AE_CDI = pd.read_csv(word_path + '/AE_content.csv')
+BE_CDI = pd.read_csv(word_path + '/BE_content.csv')
+
+condition = 'Expressive'
+CDI_frame = AE_CDI
+target_frame = vocab_filtered
+threshold = 600
+by_freq = True
+
+# lemmatize all the candidate words
+# lemma_dict = lemmatize(AE_CDI['word'].tolist())
+
+target_type = 'AE_CDI'
+
+# plot curves
+def plot_curve(threshold,target_frame,CDI_frame,seq_frame_prompted,seq_frame_unprompted,target_type):
     
     sns.set_style('whitegrid')
-    selected_frame, word_size  = filter_words(input_frame, target_frame,by_freq)
     
-    if not by_freq:
+    score_frame_unprompted, avg_values_unprompted = get_score(target_frame,seq_frame_unprompted,threshold)
+    score_frame_prompted, avg_values_prompted = get_score(target_frame,seq_frame_prompted,threshold)
+    # Plotting the line curve
+    sns.lineplot(score_frame_prompted.columns, avg_values_prompted.values, label= 'Prompted generation')
+    sns.lineplot(score_frame_unprompted.columns, avg_values_unprompted.values, label= 'Unprompted generation')
+    
+    
+    # plot CDI data
+    selected_words = CDI_frame.iloc[:, 5:-4]
         
-        score_frame = get_score(threshold,input_frame,word_size,info_lst,prompt,decoding)
-        ax = sns.lineplot(data=score_frame, x='Pseudo_month', y='Proportion of model')
     
-    # get scores based on threshold based on different thresholds
-    else:
-        result = []
-        for key, value in selected_frame.items():
-           
-            # selet target frame by frequency bands
-            score_frame = get_score(threshold,value,word_size,info_lst,prompt,decoding)
-            result.append(score_frame)
-            sns.set_style('whitegrid')
-            ax = sns.lineplot(data=score_frame, x='Pseudo_month', y='Proportion of model',label=key)
+    size_lst = []
+    month_lst = []
+    
+    n = 0
+    while n < selected_words.shape[0]:
+        size_lst.append(selected_words.iloc[n])
+        headers_list = selected_words.columns.tolist()
+        month_lst.append(headers_list)
+        n += 1
+
+    size_lst_final = [item for sublist in size_lst for item in sublist]
+    month_lst_final = [item for sublist in month_lst for item in sublist]
+    month_lst_transformed = []
+    for month in month_lst_final:
+        month_lst_transformed.append(int(month))
+    # convert into dataframe
+    data_frame = pd.DataFrame([month_lst_transformed,size_lst_final]).T
+    data_frame.rename(columns={0:'month',1:'Proportion of acquired words'}, inplace=True)
+    data_frame_final = data_frame.dropna(axis=0)
+    
+    ax = sns.lineplot(x="month", y="Proportion of acquired words", data=data_frame_final, label= target_type)
+    
+    # set the limits of the x-axis for each line
+    for line in ax.lines:
+        plt.xlim(0,36)
+        plt.ylim(0,1)
+    
+    plt.title('{} vocab (threshold = {})'.format(condition,threshold))
+    plt.xlabel('month', fontsize=15)
+    plt.ylabel('Proportion of children/models', fontsize=15)
+    plt.tick_params(axis='both', labelsize=10)
+    
+    plt.legend()
+    
+
+
+
+plot_curve(threshold,target_frame,CDI_frame,seq_frame_prompted,seq_frame_unprompted,target_type)
+
+
+def plot_curve(threshold,target_frame,CDI_frame,seq_frame_prompted,seq_frame_unprompted, by_freq, vocab_type):
+    
+    sns.set_style('whitegrid')
+    
+    overlapping_words = [col for col in target_frame[vocab_type].tolist() if col in seq_frame.index.tolist()]
+    # get the subdataframe
+    selected_frame = seq_frame.loc[overlapping_words]
+    
+    # add more scores additional lines to the frame
+    
+    extra_word_lst = [element for element in target_frame[vocab_type].tolist() if element not in overlapping_words]
+    
+    for word in extra_word_lst:
+        selected_frame.loc[word] = [0] * selected_frame.shape[1]
+        
+    # get the score based on theshold
+    score_frame_all = selected_frame.applymap(lambda x: 1 if x >= threshold else 0)
+    
+    if by_freq:
+        for freq in set(list(target_frame['group'].tolist())):
+            word_group = target_frame[target_frame['group']==freq]['word'].tolist()
+            score_frame = score_frame_all.loc[word_group]
             
-    plt.title(prompt + ' generated ' + seq_type +': ' + decoding + ', threshold: ' + str(threshold), fontsize=10)
-    plt.show()
-
-    return score_frame
-
-
-threshold = 1
-info_lst = ['MONTH','CHUNK','PROMPT','BEAM','TOPK','TEMP','TOPP','RANDOM']
-by_freq = False
-prompt = 'unprompted'
-decoding = 'TOPP'
-seq_type ='words'
-
-if seq_type == 'words':
-    input_frame = word_frame
+            avg_values = score_frame.mean()
+            
+            # Plotting the line curve
+            ax = sns.lineplot(score_frame.columns, avg_values.values, label= 'Model_' + freq)
     
-elif seq_type == 'lemmas':
-    input_frame = lemma_frame
-score_frame = plot_curve(threshold,input_frame,target_frame,by_freq,info_lst,prompt,decoding,seq_type)
+    else:
+        avg_values = score_frame_all.mean()
+        
+        # Plotting the line curve
+        sns.lineplot(score_frame_all.columns, avg_values.values, label= prompt_type + ' generation' )
+    
+    
+    
+    # plot CDI data
+    selected_words = CDI_frame.iloc[:, 5:-4]
+        
+    
+    size_lst = []
+    month_lst = []
+    
+    n = 0
+    while n < selected_words.shape[0]:
+        size_lst.append(selected_words.iloc[n])
+        headers_list = selected_words.columns.tolist()
+        month_lst.append(headers_list)
+        n += 1
 
-
+    size_lst_final = [item for sublist in size_lst for item in sublist]
+    month_lst_final = [item for sublist in month_lst for item in sublist]
+    month_lst_transformed = []
+    for month in month_lst_final:
+        month_lst_transformed.append(int(month))
+    # convert into dataframe
+    data_frame = pd.DataFrame([month_lst_transformed,size_lst_final]).T
+    data_frame.rename(columns={0:'month',1:'Proportion of acquired words'}, inplace=True)
+    data_frame_final = data_frame.dropna(axis=0)
+    
+    ax = sns.lineplot(x="month", y="Proportion of acquired words", data=data_frame_final, label='CDI')
+    
+    # set the limits of the x-axis for each line
+    for line in ax.lines:
+        plt.xlim(0,36)
+        plt.ylim(0,1)
+    
+    plt.title('{} vocab (threshold = {})'.format(condition,threshold))
+    plt.xlabel('month', fontsize=15)
+    plt.ylabel('Proportion of children/models', fontsize=15)
+    plt.tick_params(axis='both', labelsize=10)
   
+    plt.legend()
+    
+   
+plot_curve_freq(threshold,target_frame,seq_frame)
+
+plot_curve(threshold,target_frame,CDI_frame,seq_frame,by_freq)
+
+
+
+# get the accumulator model results
+file_dir = 'gold_raw_filtered.csv'    
+lang = 'EN'   
+freq_bands = ['low[5,88]', 'mid[88,256]', 'high[256,3083]']  
+freq, group_lst = group_freq(lang,file_dir)
+
+
+
