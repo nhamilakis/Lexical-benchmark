4c488686a50b391ba4257f4ced61b5aabb697a09
jliu <ecnucrystal@gmail.com>
2024-05-06 17:40:57 +0200

    update the score func

--- lm_benchmark/analysis/entrypoints/get_score.py
+++ lm_benchmark/analysis/entrypoints/get_score.py
@@ -13,8 +13,12 @@ def arguments() -> argparse.Namespace:
                         default='/Users/jliu/PycharmProjects/Lexical-benchmark/data/eval/exp/score/generation.csv')
     parser.add_argument("--est_file",
                         default='/Users/jliu/PycharmProjects/Lexical-benchmark/data/eval/exp/score/vocal_month.csv')
-    parser.add_argument("--out_path",
-                        default='/Users/jliu/PycharmProjects/Lexical-benchmark/data/eval/exp/score/adjusted_count/')
+    parser.add_argument("--test_file",
+                        default='/Users/jliu/PycharmProjects/Lexical-benchmark/data/eval/exp/test/test_set/AE_human.csv')
+    parser.add_argument("--count_all_file",
+                        default='/Users/jliu/PycharmProjects/Lexical-benchmark/data/eval/exp/score/count_all/content.csv')
+    parser.add_argument("--count_test_file",
+                        default='/Users/jliu/PycharmProjects/Lexical-benchmark/data/eval/exp/score/count_test/AE_human.csv')
     parser.add_argument("--header",default='content')
     parser.add_argument("--threshold", default=6)
     return parser.parse_args()
@@ -25,18 +29,23 @@ def main():
     args = arguments()
     gen_file = Path(args.gen_file)
     est_file = Path(args.est_file)
+    test_file = Path(args.test_file)
+    count_all_file = Path(args.count_all_file)
+    count_test_file = Path(args.count_test_file)
     header = args.header
 
     # get freq grouped by month
     count_loader = MonthCounter(
         gen_file=gen_file,
         est_file=est_file,
+        test_file=test_file,
+        count_all_file=count_all_file,
         header=header,
         threshold=args.threshold
     )
 
-    adjusted_df = count_loader.count_by_month()
-    adjusted_df.to_csv(args.out_path + header + '.csv', index=False)
+    adjusted_df = count_loader.get_count()
+    adjusted_df.to_csv(count_test_file, index=False)
     # select from the eval test
 
 
@@ -45,5 +54,34 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     main()
 
+'''
+import pandas as pd
+merged = pd.read_csv('/Users/jliu/PycharmProjects/Lexical-benchmark/data/eval/exp/score/count_all/content.csv')
+test = pd.read_csv('/Users/jliu/PycharmProjects/Lexical-benchmark/data/eval/exp/test/test_set/AE_human.csv')
 
+merged_df = pd.merge(merged, test, how='inner', left_index=True, right_on='word')
+merged_df = pd.merge(merged_df, df2, on='word', how='outer')
+
+
+
+        # Select rows where 'Index_in_df1' column is not null
+        self._selected_rows = merged_df[merged_df['word'].notnull()]
+
+'''
+
+import pandas as pd
+
+# Sample DataFrame
+data = {
+    'A': [1, 2, 3, 4],
+    'B': [5, 6, 7, 8],
+    'C': [9, 10, 11, 12]
+}
+
+df = pd.DataFrame(data)
+
+# Calculate cumulative count by rows starting from the second column to the last
+cumulative_count = df.iloc[:, 1:].cumsum(axis=1)
+
+print(cumulative_count)
 
--- lm_benchmark/analysis/score/score.py
+++ lm_benchmark/analysis/score/score.py
@@ -9,29 +9,37 @@ from .score_util import merge_df,adjust_count  # TODO: check how to load this: p
 
 
 class MonthCounter:
-    def __init__(self, gen_file: Path, est_file: Path, header: str, threshold: int):
+    def __init__(self, gen_file: Path, est_file: Path, test_file: Path, count_all_file: Path, header: str, threshold: int):
 
         if not gen_file.is_file():
             raise ValueError(f'Given file ::{gen_file}:: does not exist !!')
         if not est_file.is_file():
             raise ValueError(f'Given file ::{est_file}:: does not exist !!')
+        if not test_file.is_file():
+            raise ValueError(f'Given file ::{test_file}:: does not exist !!')
+        if not count_all_file.is_file():
+            self._merged_df = None     # initialize the merged_all as None if it doesn't exist
+            print(f'Count corpus does not exist, creating and saving it to {count_all_file}')
+        else:
+            print(f'Found count corpus from: {count_all_file}, loading ...')
+            self._merged_df = pd.read_csv(count_all_file)
 
         self._generation_csv_location = gen_file
         self._estimation_csv_location = est_file
+        self._test_csv_location = test_file
+        self._threshold = threshold
         self._header = header
         self._threshold = threshold
-        self._merged_df = None
         # Call load method to initialize dataframes
         self.__load__()
 
     def __load__(self) -> None:
         """ Load the dataset into dataframes """
-        # filter subset of the words
         self._generation_df = pd.read_csv(self._generation_csv_location)
         self._estimation_df = pd.read_csv(self._estimation_csv_location)
+        self._test_df = pd.read_csv(self._test_csv_location)
 
-
-    def __adjusted_count__(self):
+    def __adjusted_count_all__(self):
         """ Match two freq frames """
         # loop over different months
         self._gen_grouped = self._generation_df.groupby('month')
@@ -49,25 +57,43 @@ class MonthCounter:
 
         # remove useless columns
         self._merged_df = self._merged_df.drop(columns=['freq_m_x'])
-        self._merged_df.set_index('word', inplace=True)
+        #self._merged_df.set_index('word', inplace=True)
         # get cumulative frequency
-        self._merged_df = self._merged_df.cumsum(axis=1)
+        #self._merged_df = self._merged_df.cumsum(axis=1)
         return self._merged_df
 
-    def __estimate_score__(self):
-        """ estimate score based on different threholds"""
-        # check the existence of the adjusted count
+    def __adjusted_count_test__(self):
+        """ estimate score based on different thresholds"""
+        # filter the test set
+        #self._selected_rows = self._merged_df[self._test_df['word'].notnull()]
+        # Merge df1 and df2 on the index of df1 and 'Index_in_df1' column of df2
+        merged_df = self._merged_df.merge(self._test_df, how='inner', left_index=True, right_on='word')
+        # Select rows where 'Index_in_df1' column is not null
+        self._selected_rows = merged_df[merged_df['word'].notnull()]
+        return self._selected_rows
+
+
+    def __score__(self):
+        """ estimate score based on different thresholds"""
+        # filter the test set
+
+        # match the group info
 
         # apply threshold on the whole dataframe
 
-        return self._matched_CDI
+        #return self._matched_CDI
+        return None
+    def get_count(self):
+        """ Get matched data """
+        if self._merged_df is None:
+            self._merged_df = self.__adjusted_count_all__()
+        #self._selected_rows = self.__adjusted_count_test__()
+        return self._merged_df
 
-    def count_by_month(self):
+
+    def get_score(self):
         """ Get matched data """
         if self._merged_df is None:
             self._merged_df = self.__adjusted_count__()
         return self._merged_df
-
-
-
 
