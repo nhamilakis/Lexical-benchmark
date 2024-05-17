c81882ae6bfe4a26380fcf94d83a370d07faf668
jliu <ecnucrystal@gmail.com>
2024-05-17 22:16:25 +0200

    update save dir

--- lm_benchmark/datasets/entrypoints/build_human_cdi.py
+++ lm_benchmark/datasets/entrypoints/build_human_cdi.py
@@ -2,9 +2,8 @@ import argparse
 import dataclasses
 import sys
 from pathlib import Path
-
-from lm_benchmark.datasets.human_cdi import POSTypes, AGE_MIN, AGE_MAX, GoldReferenceCSV
-
+from lm_benchmark.settings import ROOT
+from lm_benchmark.datasets.human_cdi import POSTypes, AGE_DICT, GoldReferenceCSV
 
 @dataclasses.dataclass
 class BuildHumanCDI:
@@ -16,10 +15,10 @@ def arguments() -> argparse.Namespace:
     parser.add_argument('--filter-by-word-type',
                         choices=[str(s) for s in POSTypes], type=str, default=str(POSTypes.content.value),
                         help='Filter words by word_type')
-    parser.add_argument("--min-age", type=int, default=AGE_MIN)
-    parser.add_argument("--max-age", type=int, default=AGE_MAX)
-    parser.add_argument("--src_file", default='/Users/jliu/PycharmProjects/Lexical-benchmark/data/eval/corpus/raw/AE_exp.csv')
-    parser.add_argument("--target_file", default='/Users/jliu/PycharmProjects/Lexical-benchmark/data/eval/exp/test/corpus/AE_all.csv')
+    parser.add_argument("--lang", type=str, default='BE')
+    parser.add_argument("--test_type",type=str, default='exp')
+    parser.add_argument("--src_file",type=str, default= f'{ROOT}/datasets/raw/')
+    parser.add_argument("--target_file",type=str, default=f'{ROOT}/datasets/processed/CDI/')
     return parser.parse_args()
 
 
@@ -27,12 +26,15 @@ def main():
     """ Run the GoldReference loader and write results to a file """
     args = arguments()
 
-    src = Path(args.src_file)
-    target = Path(args.target_file)
+    src = Path(args.src_file).joinpath(args.lang + '_' + args.test_type + '.csv')
+    target = Path(args.target_file).joinpath(args.lang + '_' + args.test_type + '.csv')
+    print(target)
+    age_min = AGE_DICT[args.lang][0]
+    age_max = AGE_DICT[args.lang][1]
     gd_loader = GoldReferenceCSV(
         raw_csv=src,
-        age_min=args.min_age,
-        age_max=args.max_age,
+        age_min=age_min,
+        age_max=age_max,
         pos_filter_type=POSTypes(args.filter_by_word_type)
     )
     gd_loader.gold.to_csv(target, index=False)
