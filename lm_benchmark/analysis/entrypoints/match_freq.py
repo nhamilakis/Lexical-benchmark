270f57fcba9a074e6a27671c03b6fd440d106415
jliu <ecnucrystal@gmail.com>
2024-05-19 11:15:39 +0200

    concatenate group stat

--- lm_benchmark/analysis/entrypoints/match_freq.py
+++ lm_benchmark/analysis/entrypoints/match_freq.py
@@ -6,21 +6,20 @@ import numpy as np
 from pathlib import Path
 from lm_benchmark.settings import ROOT
 from lm_benchmark.analysis.freq.freq_util import bin_stats,loss,init_index,swap_index,get_freq
-
-
 def arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--CDI_file",
-                        default=f'{ROOT}/datasets/processed/CDI/AE_exp.csv')
+    parser.add_argument("--CDI_path",
+                        default=f'{ROOT}/datasets/processed/CDI/')
     parser.add_argument("--human_freq",
                         default=f'{ROOT}/datasets/processed/freq/CHILDES_adult.csv')
     parser.add_argument("--machine_freq",
                         default=f'{ROOT}/datasets/processed/freq/3200.csv')
-    parser.add_argument("--machine_CDI",
-                        default=f'{ROOT}/datasets/processed/CDI/AE_exp.csv')
+    parser.add_argument("--lang", type=str, default='AE')
+    parser.add_argument("--test_type", type=str, default='exp')
     parser.add_argument("--sampling_ratio", type=int, default=1)
-    parser.add_argument("--nbins", default=12)
+    parser.add_argument("--nbins", type=int, default=6)
     return parser.parse_args()
+
 
 def annotate_freq(CDI_file,human_freq):
     CDI_file = pd.read_csv(CDI_file)
@@ -30,7 +29,7 @@ def annotate_freq(CDI_file,human_freq):
     return cleaned_df
 
 
-def match_sample(dataref,datasam,sampling_ratio=1,nbins=12,n=100000):
+def match_sample(dataref,datasam,sampling_ratio,nbins,n=100000):
     """"Match a source distribution to a target distribution by sampling randomly from the (larger)
     source distribution in order to minimize a given loss function
     returns the index to the source distribution, the loss and various stats
@@ -54,27 +53,30 @@ def match_sample(dataref,datasam,sampling_ratio=1,nbins=12,n=100000):
         l1=loss(refstat,datastat)
         if lbest>l1:
             lbest=l1
-            #print(lbest)
             pidx,nidx=np.array(pidx1,copy=True),np.array(nidx1,copy=True)
-        #print(lbest,l1)
-    return pidx,lbest,refstat,bin_stats(datasam[pidx],nbins)
+    teststat = bin_stats(datasam[pidx], nbins)
+    refstat['set'] = 'human'
+    teststat['set'] = 'machine'
+    stat = pd.concat([refstat,teststat])
+    return pidx,lbest,stat
 
 def main():
     """ Run the GoldReference loader and write results to a file """
     args = arguments()
-    sampling_ratio = args.sampling_ratio
+    lang = args.lang
     machine_freq = Path(args.machine_freq)
-    CDI_file = Path(args.CDI_file)
+    CDI_file = Path(args.CDI_path).joinpath(lang + '_' + args.test_type + '_human.csv')
+    CDI_stat = Path(args.CDI_path).joinpath(lang + '_' + args.test_type + '_stat.csv')
     human_freq = Path(args.human_freq)
-    machine_CDI = Path(args.machine_CDI)
-
+    machine_CDI = Path(args.CDI_path).joinpath(lang + '_' + args.test_type + '_machine.csv')
     # match human-CDI and CHIDLES
     target = annotate_freq(CDI_file, human_freq)
     machine_freq = pd.read_csv(machine_freq)
     # match files
-    pidx, lbest, tstat, sstat = match_sample(target, machine_freq, sampling_ratio=sampling_ratio)
-    machine_freq[pidx].to_csv(machine_CDI)
-
+    pidx, lbest, stat = match_sample(target, machine_freq, args.sampling_ratio, args.nbins)
+    # save the files
+    machine_freq.iloc[pidx].to_csv(machine_CDI)
+    stat.to_csv(CDI_stat)
 
 
 
