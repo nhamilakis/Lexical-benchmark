298f40fa619252629525cddfb330ed5d6df5aa66
Hamilakis Nicolas <nicolas.hamilakis@ens.psl.eu>
2024-05-22 21:41:29 +0200

    syntactical corrections

--- lm_benchmark/analysis/entrypoints/adjust_count.py
+++ lm_benchmark/analysis/entrypoints/adjust_count.py
@@ -1,53 +1,56 @@
-"""convert word count into accumulated monthly count"""
+"""convert word count into accumulated monthly count."""
+
 import argparse
-import os
 import sys
 from pathlib import Path
+
 from tqdm import tqdm
+
 from lm_benchmark.analysis.utils import MonthCounter
 from lm_benchmark.settings import ROOT
 
+
 def arguments() -> argparse.Namespace:
+    """Build & Parse command-line arguments."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--gen_file",
-                        default=f'{ROOT}/datasets/processed/generation/generation.csv')
-    parser.add_argument("--est_file",
-                        default=f'{ROOT}/datasets/raw/vocal_month.csv')
-    parser.add_argument("--CDI_path", default=f'{ROOT}/datasets/processed/CDI/')
-    parser.add_argument("--freq_path",default=f'{ROOT}/datasets/processed/month_count/')
-    parser.add_argument("--test_type", default='exp')
-    parser.add_argument("--lang",default='AE')
-    parser.add_argument("--set", default='machine')
-    parser.add_argument("--header_lst",default=['unprompted_0.3','unprompted_0.6'
-                        ,'unprompted_1.0','unprompted_1.5'])
+    parser.add_argument("--gen_file", default=f"{ROOT}/datasets/processed/generation/generation.csv")
+    parser.add_argument("--est_file", default=f"{ROOT}/datasets/raw/vocal_month.csv")
+    parser.add_argument("--CDI_path", default=f"{ROOT}/datasets/processed/CDI/")
+    parser.add_argument("--freq_path", default=f"{ROOT}/datasets/processed/month_count/")
+    parser.add_argument("--test_type", default="exp")
+    parser.add_argument("--lang", default="AE")
+    parser.add_argument("--set", default="machine")
+    parser.add_argument(
+        "--header_lst", default=["unprompted_0.3", "unprompted_0.6", "unprompted_1.0", "unprompted_1.5"]
+    )
     return parser.parse_args()
 
 
-def main():
-    """ Run the GoldReference loader and write results to a file """
+def main() -> None:
+    """Run the GoldReference loader and write results to a file."""
     args = arguments()
     gen_file = Path(args.gen_file)
     est_file = Path(args.est_file)
     lang = args.lang
     header_lst = args.header_lst
-    test_file = Path(args.CDI_path).joinpath(lang + '_' + args.test_type + '_'+ args.set + '.csv')
-    score_dir = args.freq_path + lang + '/'
-    if not os.path.exists(score_dir):
-        os.makedirs(score_dir)
+    test_file = Path(args.CDI_path) / f"{lang}_{args.test_type}_{args.set}.csv"
+    score_dir = Path(f"{args.freq_path}{lang}")
+    if not score_dir.exists():
+        score_dir.mkdir(parents=True)
 
     for header in tqdm(header_lst):
-        count_test_file = Path(score_dir + header + '.csv')
-        count_all_file = Path(args.freq_path + header + '.csv')
+        count_test_file = Path(score_dir + header + ".csv")
+        count_all_file = Path(args.freq_path + header + ".csv")
         # get adjusted count grouped by month
         count_loader = MonthCounter(
-                gen_file=gen_file,
-                est_file=est_file,
-                test_file=test_file,
-                count_all_file=count_all_file,
-                count_test_file = count_test_file,
-                header= header
-            )
-        count_loader.get_count()     # get the adjusted test count
+            gen_file=gen_file,
+            est_file=est_file,
+            test_file=test_file,
+            count_all_file=count_all_file,
+            count_test_file=count_test_file,
+            header=header,
+        )
+        count_loader.get_count()  # get the adjusted test count
 
 
 if __name__ == "__main__":
--- lm_benchmark/analysis/entrypoints/match_freq.py
+++ lm_benchmark/analysis/entrypoints/match_freq.py
@@ -1,90 +1,107 @@
-"""match freq based on between human and machine cdi"""
+"""match freq based on between human and machine cdi."""
+
 import argparse
-import pandas as pd
 import sys
-import numpy as np
 from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+from lm_benchmark.analysis.freq.freq_util import bin_stats, get_freq, init_index, loss, swap_index
 from lm_benchmark.settings import ROOT
-from lm_benchmark.analysis.freq.freq_util import bin_stats,loss,init_index,swap_index,get_freq
+
+
 def arguments() -> argparse.Namespace:
+    """Build & Parse command-line arguments."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--CDI_path",
-                        default=f'{ROOT}/datasets/processed/CDI/')
-    parser.add_argument("--human_freq",
-                        default=f'{ROOT}/datasets/processed/freq/CHILDES_adult.csv')
-    parser.add_argument("--machine_freq",
-                        default=f'{ROOT}/datasets/processed/freq/3200.csv')
-    parser.add_argument("--lang", type=str, default='BE')
-    parser.add_argument("--test_type", type=str, default='exp')
+    parser.add_argument("--CDI_path", default=f"{ROOT}/datasets/processed/CDI/")
+    parser.add_argument("--human_freq", default=f"{ROOT}/datasets/processed/freq/CHILDES_adult.csv")
+    parser.add_argument("--machine_freq", default=f"{ROOT}/datasets/processed/freq/3200.csv")
+    parser.add_argument("--lang", type=str, default="BE")
+    parser.add_argument("--test_type", type=str, default="exp")
     parser.add_argument("--sampling_ratio", type=int, default=1)
     parser.add_argument("--nbins", type=int, default=6)
     return parser.parse_args()
 
 
-def annotate_freq(CDI_file,human_freq):
-    CDI_file = pd.read_csv(CDI_file)
-    human_freq = pd.read_csv(human_freq)
-    merged_df = pd.merge(CDI_file,human_freq, on='word', how='left')
-    cleaned_df = merged_df.dropna()
-    return cleaned_df
+def annotate_freq(cdi_file: Path, human_freq: Path) -> pd.DataFrame:
+    """Annotate Frequencies."""
+    cdi_data = pd.read_csv(str(cdi_file))
+    human_freq_data = pd.read_csv(str(human_freq))
+    merged_df = cdi_data.merge(human_freq_data, on="word", how="left")
+    return merged_df.dropna()
 
 
-def match_sample(dataref,datasam,sampling_ratio,nbins,n=100000):
-    """"Match a source distribution to a target distribution by sampling randomly from the (larger)
+def match_sample(
+    dataref: pd.DataFrame,
+    datasam: pd.DataFrame,
+    sampling_ratio: int,
+    nbins: int,
+    n: int = 100000,
+) -> tuple[int, int, pd.DataFrame]:
+    """Match a source distribution to a target distribution.
+
+    This is achieved by sampling randomly from the (larger)
     source distribution in order to minimize a given loss function
     returns the index to the source distribution, the loss and various stats
-    The two distributions have the same number of samples (if sampling_ratio larger than one, the returned distribution can contain more samples than the target distribution)
+    The two distributions have the same number of samples (if sampling_ratio larger than one,
+    the returned distribution can contain more samples than the target distribution).
     """
     # convert into freq_m
-    dataref = get_freq(dataref,'count')
-    datasam = get_freq(datasam,'count')
-    lenref=len(dataref)
-    lensam=len(datasam)
-    assert lenref*sampling_ratio<lensam
-    refstat=bin_stats(dataref,nbins)
-    pidx,nidx=init_index(lensam,lenref*sampling_ratio)
-    data=datasam[pidx]
-    datastat=bin_stats(data,nbins)
-    lbest=loss(refstat,datastat)
-    for i in range(n):
-        pidx1,nidx1=swap_index(pidx,nidx)
-        data=datasam[pidx1]
-        datastat=bin_stats(data,nbins)
-        l1=loss(refstat,datastat)
-        if lbest>l1:
-            lbest=l1
-            pidx,nidx=np.array(pidx1,copy=True),np.array(nidx1,copy=True)
-    teststat = bin_stats(datasam[pidx], nbins)
-    refstat['set'] = 'human'
-    teststat['set'] = 'machine'
-    stat = pd.concat([refstat,teststat])
-    return pidx,lbest,stat
+    dataref = get_freq(dataref, "count")
+    datasam = get_freq(datasam, "count")
 
-def main():
-    """ Run the GoldReference loader and write results to a file """
+    lenref = len(dataref)
+    lensam = len(datasam)
+    if not lenref * sampling_ratio < lensam:
+        raise ValueError("XXXXXXXXXXXXX")  # TODO: add a message explaining why this is an error
+
+    refstat = bin_stats(dataref, nbins)
+    pidx, nidx = init_index(lensam, lenref * sampling_ratio)
+    data = datasam[pidx]
+    datastat = bin_stats(data, nbins)
+    lbest = loss(refstat, datastat)
+
+    for _ in range(n):
+        pidx1, nidx1 = swap_index(pidx, nidx)
+        data = datasam[pidx1]
+        datastat = bin_stats(data, nbins)
+        l1 = loss(refstat, datastat)
+        if lbest > l1:
+            lbest = l1
+            pidx, nidx = np.array(pidx1, copy=True), np.array(nidx1, copy=True)
+
+    teststat = bin_stats(datasam[pidx], nbins)
+    refstat["set"] = "human"
+    teststat["set"] = "machine"
+    stat = pd.concat([refstat, teststat])
+    return pidx, lbest, stat
+
+
+def main() -> None:
+    """Run the GoldReference loader and write results to a file."""
     args = arguments()
     lang = args.lang
-    machine_freq = Path(args.machine_freq)
-    CDI_file = Path(args.CDI_path).joinpath(lang + '_' + args.test_type + '_human.csv')
-    CDI_stat = Path(args.CDI_path).joinpath(lang + '_' + args.test_type + '_stat.csv')
-    human_freq = Path(args.human_freq)
-    machine_CDI = Path(args.CDI_path).joinpath(lang + '_' + args.test_type + '_machine.csv')
-    # match human-CDI and CHIDLES
-    target = annotate_freq(CDI_file, human_freq)
-    machine_freq = pd.read_csv(machine_freq)
+
+    machine_freq_file = Path(args.machine_freq)
+    cdi_file = Path(args.CDI_path) / f"{lang}_{args.test_type}_human.csv"
+    cdi_stat_file = Path(args.CDI_path) / f"{lang}_{args.test_type}_stat.csv"
+    human_freq_file = Path(args.human_freq)
+    machine_cdi_file = Path(args.CDI_path) / f"{lang}_{args.test_type}_machine.csv"
+
+    # match human-CDI and CHILDES
+    target = annotate_freq(cdi_file, human_freq_file)
+    machine_freq = pd.read_csv(str(machine_freq_file))
+
     # match files
-    pidx, lbest, stat = match_sample(target, machine_freq, args.sampling_ratio, args.nbins)
+    pidx, _, stat = match_sample(target, machine_freq, args.sampling_ratio, args.nbins)
+
     # save the files
-    target.to_csv(CDI_file)
-    machine_freq.iloc[pidx].to_csv(machine_CDI)
-    stat.to_csv(CDI_stat)
-
-
-
+    target.to_csv(cdi_file)
+    machine_freq.iloc[pidx].to_csv(machine_cdi_file)
+    stat.to_csv(cdi_stat_file)
 
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     main()
-
-
