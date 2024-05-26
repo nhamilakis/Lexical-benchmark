8865e5ee3eae1cb5cf659c9718090e5b5a056def
jliu <ecnucrystal@gmail.com>
2024-05-27 01:04:03 +0200

    add CF analysis

--- scripts/CF_analysis.py
+++ scripts/CF_analysis.py
@@ -9,6 +9,8 @@ def parseArgs(argv):
                         help='batch dir')
     parser.add_argument('--output_dir', type=str, default=f"{ROOT}/datasets/processed/CDI/100",
                         help='unique set dir')
+    parser.add_argument('--gen_dir', type=str, default=f"{ROOT}/datasets/processed/generation/100",
+                        help='unique set dir')
     return parser.parse_args(argv)
 
 
@@ -17,12 +19,18 @@ def main(argv):
     args = parseArgs(argv)
     input_dir = Path(args.input_dir)
     output_dir = Path(args.output_dir)
+    gen_dir = Path(args.gen_dir)
     filenames = sort_files(input_dir,'txt')
+    gen_filenames = sort_files(gen_dir, 'csv')
     # load files
     files = load_files(filenames,input_dir)
+    gen_files = load_files(gen_filenames, gen_dir)
     print(f'Loading files from {input_dir}')
-    select_probe_set(files, output_dir)
+    probe_files = select_probe_set(files, output_dir)
     print(f'Saved the selected probing set to {output_dir}')
+    result = compare_scores(probe_files,gen_files)
+
+
 
 
 if __name__ == "__main__":
