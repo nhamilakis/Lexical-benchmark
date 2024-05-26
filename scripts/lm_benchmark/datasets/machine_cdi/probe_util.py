37d3818a11bd3fee958db402e40086072be14ff2
jliu <ecnucrystal@gmail.com>
2024-05-27 00:50:16 +0200

    CF analysiz

--- scripts/lm_benchmark/datasets/machine_cdi/probe_util.py
+++ scripts/lm_benchmark/datasets/machine_cdi/probe_util.py
@@ -2,9 +2,10 @@
 from pathlib import Path
 import pandas as pd
 from tqdm import tqdm
+from scipy.stats import ttest_rel
 from lm_benchmark.utils import TokenCount
-
-def sort_files(directory:Path,suffix:str)->list:
+from lm_benchmark.plot_util import tc_compute_miss_oov_rates
+def sort_files(directory:Path,suffix:str)->dict:
     # Get list of files in the directory
     files = [file.name for file in directory.iterdir() if file.name.endswith(suffix)]
     # Extract epoch and batch numbers from file names
@@ -16,7 +17,7 @@ def sort_files(directory:Path,suffix:str)->list:
     return sorted_file_names
 
 
-def load_files(file_names: list, parent_path: Path) -> list:
+def load_files(file_names: list, parent_path: Path)->TokenCount:
     """
     Load files as TC objects
 
@@ -25,56 +26,90 @@ def load_files(file_names: list, parent_path: Path) -> list:
     - parent_dir (str): Path to the common parent directory.
 
     Returns:
-    - dataframes (list): List of DataFrames read from the files.
+    - dataframes (list): List of TokwnCounts read from the files.
     """
-    dataframes = []
+    dataframes = {}
 
     for file_name in file_names:
         file_path = parent_path / file_name
-        if file_path.is_file():
+        if file_path.name.endswith('txt'):
             # load as TC object
             word_count = TokenCount.from_text_file(file_path)
-            df = word_count.df
-            dataframes.append(df)
-        else:
-            print(f"File not found: {file_path}")
+        elif file_path.name.endswith('csv'):
+            word_count = TokenCount.from_df(file_path, 'LSTM_segmented')
+
+        word_count.name = file_name[:-4]
+        dataframes[file_name[:-4]] = word_count
 
     return dataframes
 
 
-def select_probe_set(files:list,out_dir:Path)->pd.DataFrame:
+def select_probe_set(files: dict, out_dir: Path) -> dict:
     """
-    input: a list of sorted files
-    return: a list of unique probe files
+    input: a dictionary of sorted files
+    return: a dictionary of unique probe files
     """
-    # create out_path dir if there is not
+    # Create out_dir if it does not exist
     out_dir.mkdir(parents=True, exist_ok=True)
     # Iterate over batches (T-1, T, T+1) recursively
     stat_lst = []
-    for i in tqdm(range(1, len(files) - 1)):
+    dataframes = {}
+    file_keys = list(files.keys())
+    for i in tqdm(range(1, len(file_keys) - 1)):
         # Load word count CSV files for batches T-1, T, T+1
-        dfs = files[i - 1: i + 2]
-        '''
-        # remove 25% of words with lowest frequency in batch T
-        num_selected_words = max(int(len(dfs[1]) * 0.75), 1)
-        df_t_sorted = dfs[1].sort_values(by='count', ascending=True)
-        selected_words = df_t_sorted.head(num_selected_words)['word'].tolist()
-        '''
-        selected_words = dfs[1]['word'].tolist()
+        prev_file = file_keys[i - 1]
+        curr_file = file_keys[i]
+        next_file = file_keys[i + 1]
+        df_prev = files[prev_file].df
+        df_curr = files[curr_file].df
+        df_next = files[next_file].df
+
+        # Get the words from the current batch
+        selected_words = df_curr['word'].tolist()
         # Remove words overlapping with batches T-1 and T+1
-        selected_words = [word for word in selected_words if word not in dfs[0]['word'] and word not in dfs[1]['word']]
-        selected_df = dfs[1][dfs[1]['word'].isin(selected_words)]
-        # get stat for the .csv file
-        stat_lst.append([files[i],selected_df.shape[0]])
+        selected_words = [word for word in selected_words if word not in df_prev['word'].tolist() and word not in df_next['word'].tolist()]
+        selected_df = df_curr[df_curr['word'].isin(selected_words)]
+
+        # Get statistics for the CSV file
+        stat_lst.append([curr_file, selected_df.shape[0]])
+
+        # Convert it to TokenCount object
+        word_count = TokenCount()
+        word_count.name = curr_file
+        word_count.df = selected_df
+        dataframes[curr_file] = word_count
+
         # Write probe set to file
         selected_df.to_csv(out_dir / f"batch_{i}.csv", index=False)
-        print(f"Probe set for batch {files[i]} saved to {out_dir}")
+        print(f"Probe set for batch {curr_file} saved to {out_dir}")
 
-    stat_df = pd.DataFrame(stat_lst, columns=['filename', 'token_type'])
+    stat_df = pd.DataFrame(stat_lst, columns=['filename', 'token_count'])
     stat_df.to_csv(out_dir / 'stat_probe.csv', index=False)
+    return dataframes
 
-    return stat_df
+def compare_scores(probe_files:list,gen_files:list)-> dict:
+    stat = pd.DataFrame()
+    for filename,probe_tc in probe_files.items():
+        # select from the corresponding results
+        msc_prev, osc_prev, nsc_prev = tc_compute_miss_oov_rates(probe_tc,gen_files[filename.split('_')[0]+'_'+str(int(filename.split('_')[1])-1)],groupbin=1)
+        msc_cur, osc_cur, nsc_cur = tc_compute_miss_oov_rates(probe_tc,gen_files[filename],groupbin=1)
+        msc_next, osc_next, nsc_next = tc_compute_miss_oov_rates(probe_tc,gen_files[filename.split('_')[0]+'_'+str(int(filename.split('_')[1])+1)],groupbin=1)
+        stat_temp = pd.DataFrame([[msc_prev['dfreq_score'].tolist()[0],msc_prev['pmiss'].tolist()[0], osc_prev['poov'].tolist()[0], nsc_prev['pnword'].tolist()[0],
+                                   msc_cur['dfreq_score'].tolist()[0], msc_cur['pmiss'].tolist()[0], osc_cur['poov'].tolist()[0], nsc_cur['pnword'].tolist()[0],
+                                   msc_next['dfreq_score'].tolist()[0], msc_next['pmiss'].tolist()[0], osc_next['poov'].tolist()[0], nsc_next['pnword'].tolist()[0]]])
+        stat = pd.concat([stat,stat_temp])
+    stat.columns = ['freq_score_prev','pmiss_prev','poov_prev','pnword_prev',
+                    'freq_score_cur','pmiss_cur','poov_cur','pnword_cur',
+                    'freq_score_next','pmiss_next','poov_next','pnword_next']
 
+    # run stat analysis
+    result = {}
+    score_lst = ['freq_score', 'pmiss', 'poov', 'pnword']
+    for score in score_lst:
+        t_learn, p_learn = ttest_rel(stat[f'{score}_cur'], stat[f'{score}_prev'])
+        t_forget, p_forget = ttest_rel(stat[f'{score}_cur'], stat[f'{score}_next'])
+        result[f'{score}_learn'] = p_learn
+        result[f'{score}_forget'] = p_forget
 
-
+    return result
 
