69caa6814cff9fc3f0bbdbbbb2dca2a2af8f47b6
Jing-L97 <ecnucrystal@gmail.com>
2025-01-15 18:42:09 +0100

    update generate gen

--- experiments/gen/generate_gen_array.py
+++ experiments/gen/generate_gen_array.py
@@ -7,6 +7,9 @@ from pathlib import Path
 from lexical_benchmark import settings
 from lexical_benchmark.utils.format_util import *
 
+
+
+#TODO: remove the argument of the model type
 def parseargs():
     # Run parameters
     parser = argparse.ArgumentParser(description='Get the array script for generation')
@@ -16,9 +19,9 @@ def parseargs():
                       help='whether to check there exists the finished job')
     parser.add_argument('--target_model', default = 'trans',
                       help='the target model to be trained; used to check and specify the model dir')
-    parser.add_argument('--Target_dataset', default = [],
+    parser.add_argument('--target_dataset', default = [],
                       help='only load the target dataset; if empty include all')
-    parser.add_argument('--Target_month', default = [],
+    parser.add_argument('--target_month', default = [12,24,36],
                       help='only load the target month for training; if empty include all')
     parser.add_argument('--max_num', default = 0,
                       help='max number of models, if 0 include all')
@@ -28,73 +31,72 @@ def parseargs():
 
 
 
-def main(argv):
 
+def main(argv):
     # Args parser
     args = parseargs()
     root_dir: Path = settings.PATH.DATA_DIR/'models'
-
-    print('##########################')
-    print('Filter the directory list!')
-    print('##########################')
-
-    parent_dirs = list(root_dir.iterdir())
-
-    # Filter by Target_dataset if specified
-    if len(args.Target_dataset) > 0:
-        parent_dirs = [d for d in parent_dirs if d.name in args.Target_dataset]
-    # Filter by Target_month if specified
-    if len(args.Target_month) > 0:
-        parent_dirs = [d for d in parent_dirs if d.name in args.Target_month]
-
-    print('##########################')
-    print('Iterate over the directory')
-    print('##########################')
-
-    # Get the subdirectories for each parent
+    
     data_dirs = []
     model_dirs = []
-    for parent_folder in tqdm(parent_dirs):
+    
+    # Filter by dataset
+    print('Filter by the given dataset')
+    dir_filter = DirectoryFilter(root_dir)
+    dataset_dirs = dir_filter.filter_subdirs_by_name(args.target_dataset)
+    
+    for parent_folder in tqdm(dataset_dirs):
         monthly_path = parent_folder/'by_month'/args.lang
-        if monthly_path.exists():
-            # Get all month directories
-            month_dirs = get_subdirs(monthly_path)
-            
-            # Process each month directory
-            for month_dir in month_dirs:
-                # Get subdirectories under each month
-                sub_month_dirs = get_subdirs(month_dir)
-                if args.max_num > 0:
-                    # Sort subdirectories numerically and select first n
-                    sub_month_dirs = get_subdirs_by_count(sub_month_dirs,args.max_num)
-                     
-                # Process each path and check if transformed path exists
-                for original_path_parent in sub_month_dirs:
-                    # Create the transformed path
-                    original_path = original_path_parent/args.target_model
-                    # check whether model training has finished 
-                    if (original_path/'pytorch_model.bin').exists():
-                        transformed_path = Path(str(original_path).replace('models', 'gen'))
+        if not monthly_path.exists():
+            print(f"Monthly path does not exist: {monthly_path}")
+            continue
+        
+        # Filter by model type
+        print('Filter by the target month')
+        model_filter = DirectoryFilter(monthly_path)
+        target_month = [str(num) for num in args.target_month]
+        month_dirs = model_filter.filter_subdirs_by_name(target_month)
+        print(month_dirs)
+        for month_dir in month_dirs:
+            # Filter by chunk numbers
+            print('Filter by the chunk numbers')
+            chunk_filter = DirectoryFilter(month_dir)
+            target_month_dirs = chunk_filter.filter_subdirs_by_count(args.max_num)
 
-                        if str_to_bool(args.Resume):
-                            # only check the existing checkpoint when setting "Resume" as True
-                            if not (transformed_path/'gen.csv').exists():
-                                # Only add paths that don't have corresponding transformed versions
-                                data_dirs.append(original_path)
-                                model_dirs.append(transformed_path)
-                            else:
-                                print(f"The target generation already exists: {transformed_path}")
-                        else:
-                            print('Ignore the finished generation, generate from scratch')
+            for target_month_dir in target_month_dirs:
+                # Filter by target month
+                print('Filter by the model type')
+                month_filter = DirectoryFilter(target_month_dir)
+                sub_month_dirs = month_filter.filter_subdirs_by_name(args.target_model)
+            
+                # Process paths
+                for original_path in sub_month_dirs:
+                    
+                    # Check model training completion
+                    if not (original_path/'pytorch_model.bin').exists():
+                        print(f'Skip due to untrained model: {original_path}')
+                        continue
+                        
+                    transformed_path = Path(str(original_path).replace('models', 'gen'))
+                    
+                    if str_to_bool(args.Resume):
+                        if not (transformed_path/'gen.csv').exists():
                             data_dirs.append(original_path)
                             model_dirs.append(transformed_path)
+                        else:
+                            print(f"The target generation already exists: {transformed_path}")
                     else:
-                        print(f'Skip due to untrained model: {original_path}')
+                        print('Ignore the finished generation, generate from scratch')
+                        data_dirs.append(original_path)
+                        model_dirs.append(transformed_path)
     
-    file_df = pd.DataFrame([data_dirs,model_dirs]).T
-    file_df.to_csv(f'{args.OutPath}/{args.target_model}.gen',index = False, header = False)
-    print(f'Write the result to {args.OutPath}/{args.target_model}.gen')
-
+    # Save results
+    if data_dirs:  # Only save if we have results
+        file_df = pd.DataFrame([data_dirs, model_dirs]).T
+        file_df.to_csv(f'{args.OutPath}/{args.target_model}.gen', index=False, header=False)
+        print(f'Write the result to {args.OutPath}/{args.target_model}.gen')
+    else:
+        print("No matching directories found based on the given criteria")
 
 
 
