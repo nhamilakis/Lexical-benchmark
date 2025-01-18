c91cff867f0b1baea072465b06fd0939934efec3
Jing Liu <84009338+Jing-L97@users.noreply.github.com>
2025-01-18 23:32:07 +0100

    merge the hf and train util

--- src/scripts/train/hf/train_trans.py
+++ src/scripts/train/hf/train_trans.py
@@ -22,7 +22,7 @@ import os
 import sys
 import string
 import argparse
-from lexical_benchmark.utils.train_util import *
+from lexical_benchmark.utils.hf_util import *
 from lexical_benchmark.utils.format_util import str_to_bool
 import wandb
 wandb.init(mode="offline")
