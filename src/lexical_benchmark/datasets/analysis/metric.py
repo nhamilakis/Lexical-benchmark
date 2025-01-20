d4c1068c3f96b5f04f3e3ce790dbcd79dbdb3796
Hamilakis Nicolas <nicolas.hamilakis@ens.psl.eu>
2025-01-20 15:14:14 +0100

    updated analysis/metric

--- src/lexical_benchmark/datasets/analysis/metric.py
+++ src/lexical_benchmark/datasets/analysis/metric.py
@@ -1,43 +1,69 @@
-from pathlib import Path
+import functools
 
-import pandas as pd
+from lexical_benchmark.datasets import utils as dataset_utils
+from lexical_benchmark.stats import normalised_rejection_rates
 
-from lexical_benchmark import settings
-from lexical_benchmark.stats import block_average2_stat as block_stat
 
+def word_clean_fn(word: str, word_dict: dataset_utils.DictionairyCleaner) -> bool:
+    """Check if a word is in dict."""
+    return word_dict.check(word)
 
 
 class Metric:
-    """dataset metric for the given word list"""
-    def __init__(self, data:list[str],chunk_size:int=None):
+    """Dataset metric for the given word list."""
 
+    def __init__(
+        self,
+        data: list[str],
+        chunk_size: int | None = None,
+        word_dict: dataset_utils.DictionairyCleaner | None = None,
+    ) -> None:
         self.data = data
         self.chunk_size = chunk_size
 
+
+        if word_dict is None:
+            word_dict = dataset_utils.DictionairyCleaner(lang="EN")
+        self.word_clean_fn = functools.partial(word_clean_fn, word_dict=word_dict)
+
         if chunk_size:
-            # split into chunks
-            self.chunk_lst = block_stat.chunk_splitter(data, chunk_size) 
-        
+            chunks = normalised_rejection_rates.chunk_splitter(data, chunk_size)
+            self.stats = normalised_rejection_rates.clean_chunk_list(chunks=chunks, filter_fn=self.word_clean_fn)
+            self.chunked = True
+        else:
+            self.chunked = False
+            self.stats = normalised_rejection_rates.word_clean_chunk(data, filter_fn=self.word_clean_fn)
 
-    
-    def compute_ttr(self)->float:
-        """get token/type ratio of the input string"""
-        return block_stat.type_token_chunk(self.chunk_lst)
+    def compute_ttr(self) -> float:
+        """Get token/type ratio of the input string."""
+        if self.chunked:
+            return self.stats.mean_type_token_ratio()
+        return self.stats.type_token_ratio()
 
-    
+    def compute_type_rej_rate(self) -> float:
+        """Get token/type ratio of the input string."""
+        if self.chunked:
+            return self.stats.mean_type_rejection_rate()
+        return self.stats.type_rejection_rate()
 
-    def compute_type_rej_rate(self)->float:
-        """get token/type ratio of the input string"""
-        
-        return block_stat.clean_chunk_list(
-                chunks=self.chunk_list, filter_fn=word_clean_fn
-            )
-        
+    def compute_token_rej_rate(self) -> float:
+        """Get token/type ratio of the input string."""
+        if self.chunked:
+            return self.stats.mean_token_rejection_rate()
+        return self.stats.token_rejection_rate()
 
-    def compute_CDI(self,threshold:int,CDI_words:list)->float:
-        """get average CDI scores of the given word list"""
+
+    def compute_CDI(self, threshold: int, CDI_words: list) -> float:
+        """Get average CDI scores of the given word list."""
 
         return None
 
-    
 
+"""
+USAGE:
+word_dict = dataset_utils.DictionairyCleaner(lang="EN")
+
+m1 = Metric(..., word_dict=word_dict)
+m2 = Metric(..., word_dict=word_dict)
+m3 = Metric(..., word_dict=word_dict)
+"""
