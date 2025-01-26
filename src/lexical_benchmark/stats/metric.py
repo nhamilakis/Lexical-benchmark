f20f5feee9d41d6a4131b3dce9f3a897c7c40ab8
Jing Liu <84009338+Jing-L97@users.noreply.github.com>
2025-01-27 00:49:30 +0100

    update the metric class

--- src/lexical_benchmark/stats/metric.py
+++ src/lexical_benchmark/stats/metric.py
@@ -8,9 +8,9 @@ from lexical_benchmark.datasets import utils as dataset_utils
 from lexical_benchmark.stats import normalised_rejection_rates
 
 
-def load_dict(dataset_name:str):
+def load_dict(dataset_name: str):
     """Load dictionary based on different datasets."""
-    if dataset_name=='child':
+    if dataset_name == "child":
         print("Append en_dict with adult input")
         dataset = childes.CHILDESDataset()
         childes_adult_extras_lexique = childes.CHILDESExtrasLexicon(dataset)
@@ -24,123 +24,75 @@ def load_dict(dataset_name:str):
     return en_dict
 
 
-
 def word_clean_fn(word: str, word_dict: dataset_utils.DictionairyCleaner) -> bool:
     """Check if a word is in dict."""
     return word_dict.check(word)
 
 
-class Metric1:
-    """Dataset metric for the given word list."""
-
+class Metric:
     def __init__(
         self,
         data: list[str],
+        temp: str = None,
+        metric_lst: list = None,
+        threshold: int = None,
+        CDI_words: list = None,
         chunk_size: int | None = None,
         word_dict: dataset_utils.DictionairyCleaner | None = None,
     ) -> None:
-        self.data = data
+        self.temp = temp
+        self.metric_lst = metric_lst or []
+        self.threshold = threshold
+        self.CDI_words = CDI_words or []
         self.chunk_size = chunk_size
+
+        if isinstance(data[0], (str, bytes)):
+            self.data = [word for sent in data for word in str(sent).split()]
+        else:
+            self.data = data
 
         if word_dict is None:
             word_dict = dataset_utils.DictionairyCleaner(lang="EN")
         self.word_clean_fn = functools.partial(word_clean_fn, word_dict=word_dict)
 
         if chunk_size:
-            chunks = normalised_rejection_rates.chunk_splitter(data, chunk_size)
+            chunks = normalised_rejection_rates.chunk_splitter(self.data, chunk_size)
             self.stats = normalised_rejection_rates.clean_chunk_list(chunks=chunks, filter_fn=self.word_clean_fn)
             self.chunked = True
         else:
             self.chunked = False
-            self.stats = normalised_rejection_rates.word_clean_chunk(data, filter_fn=self.word_clean_fn)
+            self.stats = normalised_rejection_rates.word_clean_chunk(self.data, filter_fn=self.word_clean_fn)
 
     def compute_ttr(self) -> float:
-        """Get token/type ratio of the input string."""
         if self.chunked:
             return self.stats.mean_type_token_ratio()
         return self.stats.type_token_ratio()
 
     def compute_type_rej_rate(self) -> float:
-        """Get token/type ratio of the input string."""
         if self.chunked:
             return self.stats.mean_type_rejection_rate()
         return self.stats.type_rejection_rate()
 
     def compute_token_rej_rate(self) -> float:
-        """Get token/type ratio of the input string."""
         if self.chunked:
             return self.stats.mean_token_rejection_rate()
         return self.stats.token_rejection_rate()
 
-    def compute_CDI(self, threshold: int, CDI_words: list) -> float:
-        """Get average CDI scores of the given word list."""
+    def compute_CDI(self) -> float:
         # adjust counts by coefficients
-
+        
         return None
 
-
-class Metric:
-   def __init__(
-       self,
-       data: list[str],
-       temp: str = None,
-       metric_lst: list = None,
-       threshold: int = None,
-       CDI_words: list = None,
-       chunk_size: int | None = None,
-       word_dict: dataset_utils.DictionairyCleaner | None = None,
-   ) -> None:
-       self.temp = temp
-       self.metric_lst = metric_lst or []
-       self.threshold = threshold 
-       self.CDI_words = CDI_words or []
-       self.chunk_size = chunk_size
-       
-       if isinstance(data[0], (str, bytes)):
-           self.data = [word for sent in data for word in str(sent).split()]
-       else:
-           self.data = data
-           
-       if word_dict is None:
-           word_dict = dataset_utils.DictionairyCleaner(lang="EN")
-       self.word_clean_fn = functools.partial(word_clean_fn, word_dict=word_dict)
-       
-       if chunk_size:
-           chunks = normalised_rejection_rates.chunk_splitter(self.data, chunk_size)
-           self.stats = normalised_rejection_rates.clean_chunk_list(chunks=chunks, filter_fn=self.word_clean_fn)
-           self.chunked = True
-       else:
-           self.chunked = False
-           self.stats = normalised_rejection_rates.word_clean_chunk(self.data, filter_fn=self.word_clean_fn)
-
-   def compute_ttr(self) -> float:
-       if self.chunked:
-           return self.stats.mean_type_token_ratio()
-       return self.stats.type_token_ratio()
-
-   def compute_type_rej_rate(self) -> float:
-       if self.chunked:
-           return self.stats.mean_type_rejection_rate()
-       return self.stats.type_rejection_rate()
-
-   def compute_token_rej_rate(self) -> float:
-       if self.chunked:
-           return self.stats.mean_token_rejection_rate()
-       return self.stats.token_rejection_rate()
-
-   def compute_CDI(self) -> float:
-       # adjust counts by coefficients
-       return None
-
-   def compute_metrics(self) -> list:
-       row = [self.temp]
-       row.extend([
-           self.compute_ttr() if "type_token_ratio" in self.metric_lst else None,
-           self.compute_type_rej_rate() if "rej_type_rate" in self.metric_lst else None,
-           self.compute_CDI() if "CDI" in self.metric_lst else None
-       ])
-       return row
-
+    def compute_metrics(self) -> list:
+        row = [self.temp]
+        row.extend(
+            [
+                self.compute_ttr() if "type_token_ratio" in self.metric_lst else None,
+                self.compute_type_rej_rate() if "rej_type_rate" in self.metric_lst else None,
+                self.compute_CDI() if "CDI" in self.metric_lst else None,
+            ]
+        )
+        return row
 
 
 class SigmoidFitter:
