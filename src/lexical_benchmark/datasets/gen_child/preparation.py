679c126ae5c543b94ea060b57ce57563c9eee14c
Jing Liu <84009338+Jing-L97@users.noreply.github.com>
2025-01-28 15:36:23 +0100

    debug gen merging

--- src/lexical_benchmark/datasets/gen_child/preparation.py
+++ src/lexical_benchmark/datasets/gen_child/preparation.py
@@ -28,7 +28,7 @@ class GenerationMerger:
 
                             if gen_path_year.exists():
                                 gen_path = gen_path_year
-                            elif gen_path.exists():
+                            elif gen_path.exists() and self.hour_per_year == 1000:
                                 gen_path = gen_path  # Use regular gen_path
                             else:
                                 print(f"No generated file in {model}. Skip")
